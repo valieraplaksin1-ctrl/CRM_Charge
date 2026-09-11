a961eba11c0c0cff9714bd02070dcb7020bf7d51
valieraplaksin1-ctrl <valieraplaksin1@gmail.com>
2026-09-11 03:25:34 +0300

    Добавление версионирования для отключения кеша админки

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_session import Session
 from config import BOT_TOKEN, WEB_APP_URL, FLASK_ENV, DEBUG
 from database import *
 import os
+import time
 from functools import wraps
 
 app = Flask(__name__)
@@ -106,7 +107,7 @@ def admin():
         
         return jsonify({'success': False}), 400
     
-    return render_template('admin.html')
+    return render_template('admin.html', v=int(time.time()))
 
 @app.route('/dashboard')
 @login_required
